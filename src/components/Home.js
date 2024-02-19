469386b0b5e964a92f5ab5f78c283eadf8a3508d
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-20 01:14:20 +0300

    create the Home component and add some dummy data

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='container'>
+        <h1 className='center'>Home</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et luctus mi mauris nec dui. Cras ultricies mi eu turpis hendrerit fringilla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, </p>
+        </div>
+    );
+    }
+
+export default Home;
