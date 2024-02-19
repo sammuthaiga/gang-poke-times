d254b0c8ad333d237e3af1b0478764d98cf5b9ae
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-20 02:57:29 +0300

    add route path for Signin page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
-import { BrowserRouter,Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contacts from './components/Contacts';
+import Signin from './components/Signin';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <Router>
         <div className="App">
           <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contacts" element={<Contacts />} />
+            <Route path="/signin" element={<Signin />} />
           </Routes>
         </div>
-      </BrowserRouter>
+      </Router>
     );
   }
 }
