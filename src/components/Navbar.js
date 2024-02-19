e190fcb04d285b1578830a1bf456d6faaf3e0dd9
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-20 01:46:24 +0300

    Import Link, Navlink from react-router-dom and change the <a href> tags to <Link to=''>. This prevents the default behaviour off sending a request to the server on click but allows React to intercept. The <NavLink> adds a class of active when the user clicks on the link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,14 @@
 import React from 'react';
-
+import { Link, NavLink } from 'react-router-dom';
 const Navbar = () => {
     return (
         <nav className='nav-wrapper red darken-3'>
             <div className='container'>
                 <a href='/' className='brand-logo'>Poke' Times</a>
                 <ul className='right'>
-                    <li><a href='/'>Home</a></li>
-                    <li><a href='/about'>About</a></li>
-                    <li><a href='/contacts'>Contacts</a></li>
+                    <li><Link to='/'>Home</Link></li>
+                    <li><NavLink to='/about'>About</NavLink></li>
+                    <li><NavLink to='/contacts'>Contacts</NavLink></li>
                 </ul>
             </div>
         </nav>
