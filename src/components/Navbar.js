0b6a0ac488415558ce9df5ebfc3146839e08f554
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-20 03:07:42 +0300

    delete the withRouter HOC provided by react-router-dom

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Link, NavLink, withRouter } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
