051a54041c260329852fc087c20000e7413b57f0
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-20 04:55:53 +0300

    create a new branch ft-route-parameters
    create a new class component Post.js

---
+++ src/components/Post.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Post extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Single Post page. Will need state so it a  Class Component</h1>
+      </div>
+    );
+  }
+}
+
+export default Post;
