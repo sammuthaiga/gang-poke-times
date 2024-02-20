5e3af26c10ce6eac940f40496e0545a253d9bd10
Samuel <samuel.muthaiga@student.moringaschool.com>
2024-02-20 04:42:38 +0300

    feat: import and the Rainbow.js HOC and wrap in it the Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Rainbow from '../hoc/Rainbow'
 class Home extends Component {
     state = {
         posts: [ ]
     }
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(res => {
+            .then(res => {                
                 console.log(res);
                 this.setState({
                     posts: res.data.slice(0, 10)
@@ -17,12 +18,30 @@ class Home extends Component {
             });
     }
     render() {
+        const { posts } = this.state;
+        const postList = posts.length ? (
+            posts.map(post => {
+                return (
+                    <div className='post card' key={post.id}>
+                        <div className='card-content'>
+                            <span className='card-title'>{post.title}</span>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                );
+            })
+        ) : (
+            <div className='center'>No posts yet</div>
+        );
         return (
             <div className='container'>
                 <h1 className='center'>Home</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et luctus mi mauris nec dui. Cras ultricies mi eu turpis hendrerit fringilla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, </p>
+                <h4 className='center'>
+                    {postList}
+                </h4>
+
             </div>
         );
     }
 }
-export default Home;
+export default Rainbow(Home);
